ff4797ae19900b79ba239c8a6ab45941c2795c40
BieliaievVlad <vlad.s.bieliaiev@gmail.com>
2025-07-20 10:42:19 +0300

    Initial commit

---
+++ src/main/java/com/bieliaiev/tarkov_pmc_tools/TarkovPmcToolsApplication.java
@@ -0,0 +1,13 @@
+package com.bieliaiev.tarkov_pmc_tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarkovPmcToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarkovPmcToolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bieliaiev/tarkov_pmc_tools/TarkovPmcToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bieliaiev.tarkov_pmc_tools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarkovPmcToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
